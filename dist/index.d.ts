781671ea1337b20d34bde762cdefcbf25a36ca39
Remco Stoeten <remcostoeten@hotmail.com>
2024-02-04 16:16:37 +0100

    manually add types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,15 @@
-declare const VisualizeComponent: any;
+import { ReactNode } from "react";
+/**
+ * Props for the visualize component.
+ */
+type visualizeProps = {
+    showAlert?: boolean;
+    children?: ReactNode;
+    componentName?: string;
+    width?: string;
+    color?: string;
+    borderTypes?: 'dashed' | 'solid';
+    invert?: boolean;
+};
+export declare const VisualizeComponent: ({ borderTypes, showAlert, children, width, color, invert }: visualizeProps) => any;
 export default VisualizeComponent;
